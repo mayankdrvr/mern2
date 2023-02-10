6d10248763a8aa088645e4563260e4eb0389253d
Mayank Arora <mayankarora.email@gmail.com>
2023-02-10 10:45:56 +0530

    Add files via upload

---
+++ client/build/precache-manifest.b69390fe90146ed6e0857a9462f41919.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d58450f32dcec567846e282af173f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93af2f2d76a00dd593e6",
+    "url": "/static/css/2.de8fc4a5.chunk.css"
+  },
+  {
+    "revision": "008220217df14d23e9b1",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "93af2f2d76a00dd593e6",
+    "url": "/static/js/2.2a8554ec.chunk.js"
+  },
+  {
+    "revision": "27e38ad381e13b0c7f63849bb7798739",
+    "url": "/static/js/2.2a8554ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "008220217df14d23e9b1",
+    "url": "/static/js/main.c6d08244.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
